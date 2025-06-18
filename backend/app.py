58f21036315556276532b410ebe00eaeb15e3552
kyiv14 <kyiv14@gmail.com>
2025-06-18 23:45:41 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -39,3 +39,8 @@ if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
